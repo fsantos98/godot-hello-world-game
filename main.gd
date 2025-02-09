61d4241051c8e7450622ed79296a61a2f3479a7e
fsantos98 <franciscoslb.98@hotmail.com>
2025-02-09 15:40:04 +0000

    add all files

---
+++ main.gd
@@ -0,0 +1,49 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+func _on_mob_timer_timeout() -> void:
+	# Create a new mob
+	var mob = mob_scene.instantiate()
+
+	# Choose a random location on Path2D
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	mob.position = mob_spawn_location.position
+	
+	# Set the mobs direction perpendicular to the path direction
+	var direction = mob_spawn_location.rotation + PI / 2
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(randf_range(150.0, 250.0), 0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
