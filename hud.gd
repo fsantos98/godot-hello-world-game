ec867a4f17f82ebb8c329fb51060d9e5fd3f7960
fsantos98 <franciscoslb.98@hotmail.com>
2025-02-09 22:59:36 +0000

    Update Start button position. Free mobs on game over. Show HUD information (score and message status)

--- hud.gd
+++ hud.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+# Notifies "Main" node that the button has been pressed
 signal start_game
 
 # Called when the node enters the scene tree for the first time.
--- main.gd
+++ main.gd
@@ -5,22 +5,25 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func game_over() -> void:
+	get_tree().call_group("mobs", "queue_free")
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
@@ -28,6 +31,7 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout() -> void:
 	# Create a new mob
